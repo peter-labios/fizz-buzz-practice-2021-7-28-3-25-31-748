6fddb64c9c5fb546ab11f6be65c9d1351b4c8ef0
labiope <peter.anton.labios@oocl.com>
2022-09-01 14:37:37 +0800

    test: newly added test case passed: now returning buzz whizz when given a multiple of 5 and 7. refactor: refactored if statements

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,22 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int i) {
+        if(i%5 == 0 && i%7 == 0){
+            return "Buzz Whizz";
+        }
         if(i%3 == 0 && i%7 == 0){
             return "Fizz Whizz";
-        } else if(i%3 == 0 && i%5 == 0){
+        }
+        if(i%3 == 0 && i%5 == 0){
             return "Fizz Buzz";
-        } else if(i%3 == 0){
+        }
+        if(i%3 == 0){
             return "Fizz";
-        } else if(i%5 == 0){
+        }
+        if(i%5 == 0){
             return "Buzz";
-        } else if(i%7==0){
+        }
+        if(i%7==0){
             return "Whizz";
         }
         return String.valueOf(i);
