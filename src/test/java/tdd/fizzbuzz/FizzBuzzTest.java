63fde8a5f5e2c92fb313395dd4a16f7d0ea9daf9
labiope <peter.anton.labios@oocl.com>
2022-09-01 14:08:00 +0800

    test: should return 1 when given 1 failed

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+     void should_return_1_when_countOff_given_1(){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+        String actual = fizzBuzz.countOff(1);
+        assertEquals(expected, actual);
+    }
 }
