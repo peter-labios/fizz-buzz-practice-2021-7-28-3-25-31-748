7449dbef7e537d5f4c0aff968039cc82db92af45
labiope <peter.anton.labios@oocl.com>
2022-09-01 14:28:52 +0800

    test: newly added test case passed. now returning fizz buzz when given a multiple of 3 and 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,9 @@ package tdd.fizzbuzz;
 public class FizzBuzz {
 
     public String countOff(int i) {
-        if(i%3 == 0){
+        if(i%3 == 0 && i%5 == 0){
+            return "Fizz Buzz";
+        } else if(i%3 == 0){
             return "Fizz";
         } else if(i%5 == 0){
             return "Buzz";
